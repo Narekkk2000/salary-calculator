eaba1580da85e045ef7bb610ddc51d88fddbe199
Narekkk2000 <narek.sargsyan.2023@gmail.com>
2026-04-17 10:52:20 +0400

    changed input font-size

--- src/components/shared/InputField.vue
+++ src/components/shared/InputField.vue
@@ -35,7 +35,7 @@ const displayValue = computed(() => {
 </script>
 
 <template>
-  <BaseFlex col justify="center" align="start" class="gap-[20px]">
+  <BaseFlex justify="center" align="start" class="gap-[20px]">
     <BaseText class="ml-2 min-w-36" :style="{ marginTop: labelMarginTop ?? '0px', marginBottom: labelMarginBottom ?? '0px' }" variant="sub-hint" color="CONTENT">
       {{label}}
     </BaseText>
@@ -48,13 +48,14 @@ const displayValue = computed(() => {
       </div>
       <input
           v-bind="$attrs"
+          
           :type="hasPrefix ? 'text' : (type ?? 'number')"
           :value="displayValue"
           :placeholder="placeholder"
           :min="min"
           :max="max"
           :step="step ?? 1"
-          class="file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-sm border bg-transparent px-3 py-1 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:shadow-sm aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
+          class="file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-sm border bg-transparent px-3 py-1 text-[16px] md:text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:shadow-sm aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
           :class="{
             'pl-8': $slots.prefix,
             'pr-14': $slots.suffix,
